68ee7f9d75953dca4ef11a1557620bddfb927c36
1lavya6774 <eklavya6774@gmail.com>
2026-03-23 23:28:37 +0530

    dom

---
+++ dom.js
@@ -0,0 +1,21 @@
+// can change the all the html elemets,attributes,css styles,remove exisiting html,create a new event in html,react to all html files 
+const result=document.getElementById("eg");
+console.log(result);
+result.innerHTML="</br>HelloWorld</br>";
+result.style.color="red";
+const result=document.getElementsByClassName("p1");
+result[1].innerHTML="gupta";
+result[0].innerHTML="eklavya";
+// elementsbytagname
+
+
+
+
+
+// quaryselector
+
+
+
+//ye fix krna h
+const result=document.getElementById("jk").getAttribute("class","eg1");
+document.getElementById("p1").innerHTML=result;
