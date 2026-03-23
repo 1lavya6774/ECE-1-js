0c547a2657503ff0e2d07622adde5e3bd9475f21
1lavya6774 <eklavya6774@gmail.com>
2026-03-23 23:29:13 +0530

    event

---
+++ events.js
@@ -0,0 +1,22 @@
+function onClick(){
+    console.log("on click event is working");
+}
+
+function onMouseOver(){
+    console.log("on mouse over event is working");
+}   
+function onMouseOut(){
+    console.log("on mouse out event is working");
+}
+function onMouseDown(){
+    console.log("on mouse down event is working");
+}
+function onMouseUp(){
+    console.log("on mouse up event is working");
+}   
+function onMouseMove(){
+    console.log("on mouse move event is working");
+}
+function onDoubleClick(){    
+    console.log("on double click event is working");
+}
